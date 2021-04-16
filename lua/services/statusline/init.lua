b3bb62efe0d2d3c9cb8fcf92ab623c29d10aa891
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-16 16:03:03 -0500

    bug fix #0

--- lua/services/statusline/init.lua
+++ lua/services/statusline/init.lua
@@ -1,7 +1,7 @@
 
 -- imports
 -- TrueZen
-local service = require("service123")
+-- local service = require("service123")
 
 -- API
 
@@ -18,26 +18,43 @@ Is_statusline_shown = true
 
 --end
 
+local cmd = vim.cmd
+
+function statusline_true()		-- show
+	-- turn status line on
+	cmd("set laststatus=2 showtabline=2")
+end
+
+function statusline_false()		-- don't show
+	-- turn status line off
+	cmd("set laststatus=0 showtabline=0")
+end
+
+
+
+
 local function toggle()
 
 	if (Is_statusline_shown == true) then
-		service.statusline_false()
+		-- service.statusline_false()
+		statusline_true()
 		Is_statusline_shown = false
 	elseif (Is_statusline_shown == false) then
-		service.statusline_true()
+		-- service.statusline_true()
+		statusline_false()
 		Is_statusline_shown = true
 	else
 		-- nothing
 	end
 end
 
-local function statusline_true()
-	service.statusline_true()
-end
+-- local function statusline_true()
+-- 	service.statusline_true()
+-- end
 
-local function statusline_false()
-	service.statusline_false()
-end
+-- local function statusline_false()
+-- 	service.statusline_false()
+-- end
 
 
 function main(option)
