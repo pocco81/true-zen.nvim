547f7c0c5850d96f3dc49e5550e2a01236e12be6
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-17 13:34:32 -0500

    testing new eval

--- lua/services/statusline/init.lua
+++ lua/services/statusline/init.lua
@@ -5,22 +5,24 @@ local service = require("services.statusline.service")
 -- local api = vim.api
 local cmd = vim.cmd
 
--- vars
-Is_statusline_shown = true
 
-local function toggle()
-
-	if (Is_statusline_shown == true) then
-		service.statusline_false()
-		Is_statusline_shown = false
-	elseif (Is_statusline_shown == false) then
-		service.statusline_true()
-		Is_statusline_shown = true
-	else
-		-- nothing
-	end
+if (toggle_line_show) then		-- if var exists
+	-- global to script
+	toggle_line_show = 0
+	show = toggle_line_show
+else		-- var does not exist
+	-- global to script
+	show = vim.api.nvim_eval("&laststatus > 0 || &showtabline > 0")
+	-- returns 1 if statusline shown
+	-- returns 0 if statusline is hidden
+	-- check this: echo &laststatus > 0 || &showtabline > 0
 end
 
+local function is_shown()
+	return show
+end
+
+-- show and hide statusline funcs
 local function statusline_true()
 	service.statusline_true()
 end
@@ -29,19 +31,29 @@ local function statusline_false()
 	service.statusline_false()
 end
 
--- reset state on resume / focus gained
-function resume()
 
-	local to_cmd = "echo 'Vim was resumen, current status is -"..tostring(Is_statusline_shown).."-'"
-	cmd(to_cmd)
-	if (Is_statusline_shown == true) then
+local function toggle()
+
+	if (is_shown() == 1) then
+		service.statusline_false()
+	elseif ((is_shown() == 0)) then
 		service.statusline_true()
-	elseif (Is_statusline_shown == false) then
+	else
+		-- nothing
+	end
+end
+
+function resume()
+	if (is_shown() == 1) then
+		service.statusline_true()
+	elseif ((is_shown() == 0)) then
 		service.statusline_false()
 	else
 		-- nothing
 	end
 end
+
+
 
 function main(option)
 	
@@ -58,6 +70,7 @@ function main(option)
 	end
 end
 
+
 vim.api.nvim_exec([[
 	augroup toggle_statusline
 		autocmd!
@@ -69,6 +82,65 @@ vim.api.nvim_exec([[
 return {
 	main = main
 }
+
+
+-- vars
+-- local Is_statusline_shown = true
+
+-- local function toggle()
+
+-- 	if (Is_statusline_shown == true) then
+-- 		service.statusline_false()
+-- 		Is_statusline_shown = false
+-- 	elseif (Is_statusline_shown == false) then
+-- 		service.statusline_true()
+-- 		Is_statusline_shown = true
+-- 	else
+-- 		-- nothing
+-- 	end
+-- end
+
+
+-- reset state on resume / focus gained
+-- function resume()
+
+-- 	local to_cmd = "echo 'Vim was resumen, current status is -"..tostring(Is_statusline_shown).."-'"
+-- 	cmd(to_cmd)		-- working
+-- 	if (Is_statusline_shown == true) then
+-- 		service.statusline_true()
+-- 	elseif (Is_statusline_shown == false) then
+-- 		service.statusline_false()
+-- 	else
+-- 		-- nothing
+-- 	end
+-- end
+
+-- function main(option)
+	
+-- 	option = option or 0
+
+-- 	if (option == 0) then			-- toggle statuline (on/off)
+-- 		toggle()
+-- 	elseif (option == 1) then		-- show status line
+-- 		statusline_true()
+-- 	elseif (option == 2) then
+-- 		statusline_false()
+-- 	else
+-- 		-- not recognized
+-- 	end
+-- end
+
+-- vim.api.nvim_exec([[
+-- 	augroup toggle_statusline
+-- 		autocmd!
+-- 		autocmd VimResume,FocusGained * lua resume()
+-- 	augroup END
+-- ]], false)
+
+
+-- return {
+-- 	main = main
+-- }
 
 
 
