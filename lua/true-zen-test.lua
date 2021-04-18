3e206ce87c60c1eb1656bc91d5f29836b6a0f890
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-18 17:00:18 -0500

    test 2

---
+++ lua/true-zen-test.lua
@@ -0,0 +1,14 @@
+
+
+local cmd = vim.cmd
+
+function setup(custom_opts)
+	cmd("echo 'TrueZen was setup'")
+	-- require("config").set_options(custom_opts)
+	-- if (opts.true_false_commands == true) then
+	-- 	cmd("command! TZStatuslineT lua main(0, 1)")
+	-- 	cmd("command! TZStatuslineF lua main(0, 2)")
+	-- else
+	-- 	-- do nothing
+	-- end
+end
