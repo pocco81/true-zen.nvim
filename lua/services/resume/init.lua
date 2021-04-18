5d32e3503b27547b024a1e6e6c7500cf8fbebb35
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-18 13:02:36 -0500

    test 1

--- lua/services/resume/init.lua
+++ lua/services/resume/init.lua
@@ -4,9 +4,9 @@
 vim.api.nvim_exec([[
 	augroup toggle_statusline
 		autocmd!
-		autocmd VimResume,FocusGained * lua require'services.top'.resume()
-		autocmd VimResume,FocusGained * lua require'services.bottom'.resume()
-		autocmd VimResume,FocusGained * lua require'services.left'.resume()
+		autocmd VimResume,FocusGained * silent lua require'services.top'.resume()
+		autocmd VimResume,FocusGained * silent lua require'services.bottom'.resume()
+		autocmd VimResume,FocusGained * silent lua require'services.left'.resume()
 	augroup END
 ]], false)
 
