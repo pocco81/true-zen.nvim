458643960081c506e48408c61b870da4ddfe6cc6
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-17 23:09:37 -0500

    test 20

--- lua/true-zen.lua
+++ lua/true-zen.lua
@@ -5,15 +5,15 @@ local opts = require("config").options
 local statusline = require("services.statusline0.init")
 local cmd  = vim.cmd
 
-function setup(custom_opts)
-	require("config").set_options(custom_opts)
+-- function setup(custom_opts)
+-- 	require("config").set_options(custom_opts)
 	-- if (opts.true_false_commands == true) then
 	-- 	cmd("command! TZStatuslineT lua main(0, 1)")
 	-- 	cmd("command! TZStatuslineF lua main(0, 2)")
 	-- else
 	-- 	-- do nothing
 	-- end
-end
+-- end
 
 -- 	if (opts.setup_message == true) then
 -- 		cmd("echo 'TrueZen.nvim was set up...'")		-- working...
@@ -23,6 +23,13 @@ end
 -- 		-- do nothing
 -- 	end
 -- end
+
+if (opts.true_false_commands == true) then
+	cmd("command! TZStatuslineT lua main(0, 1)")
+	cmd("command! TZStatuslineF lua main(0, 2)")
+else
+	-- do nothing
+end
 
 
 function main(option, command_option)
