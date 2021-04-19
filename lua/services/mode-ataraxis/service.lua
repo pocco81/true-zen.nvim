92b146fd0368f0d4ee0036dc50757d59b23eac78
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-19 16:21:02 -0500

    test 5

--- lua/services/mode-ataraxis/service.lua
+++ lua/services/mode-ataraxis/service.lua
@@ -5,6 +5,12 @@ local mode_minimalist = require("services.mode-minimalist.init")
 local cmd = vim.cmd
 
 
+local function fillchars()
+	cmd("setlocal fillchars+=vert:\ ")
+	cmd("setlocal fillchars+=stl:\ ")
+	cmd("setlocal fillchars+=stlnc:\ ")
+end
+
 function ataraxis_true()		-- show
 	mode_minimalist.main(1)
 end
@@ -14,13 +20,24 @@ function ataraxis_false()		-- don't show
 	-- local padding_cmd = "vertical resize "..cmd_settings.map_settings["ataraxis"]["left_right_padding"]..""
 	local padding_cmd = "vertical resize "..opts["ataraxis"]["left_right_padding"]..""
 
+	-- left buffer
 	cmd("leftabove vnew")
 	cmd(padding_cmd)
 	cmd("setlocal buftype=nofile bufhidden=wipe nomodifiable nobuflisted noswapfile nocursorline nocursorcolumn nonumber norelativenumber noruler noshowmode noshowcmd laststatus=0")
+	fillchars()
+
+	-- middle buffer
 	cmd("wincmd l")
+
+	-- right buffer
 	cmd("vnew")
+	cmd(padding_cmd)
 	cmd("setlocal buftype=nofile bufhidden=wipe nomodifiable nobuflisted noswapfile nocursorline nocursorcolumn nonumber norelativenumber noruler noshowmode noshowcmd laststatus=0")
+	fillchars()
+
+
 	cmd("wincmd h")
+	fillchars()
 	mode_minimalist.main(2)
 end
 
