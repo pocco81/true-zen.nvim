e8abaa93aec2d4e5111cdf097531567a41b6eb99
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-20 22:07:55 -0500

    testing winenter opt

--- lua/services/resume/init.lua
+++ lua/services/resume/init.lua
@@ -4,17 +4,11 @@
 vim.api.nvim_exec([[
 	augroup toggle_statusline
 		autocmd!
-		autocmd VimResume,FocusGained * silent lua require'services.top'.resume()
-		autocmd VimResume,FocusGained * silent lua require'services.bottom'.resume()
-		autocmd VimResume,FocusGained * silent lua require'services.left'.resume()
+		autocmd VimResume,FocusGained,WinEnter * silent lua require'services.top'.resume()
+		autocmd VimResume,FocusGained,WinEnter * silent lua require'services.bottom'.resume()
+		autocmd VimResume,FocusGained,WinEnter * silent lua require'services.left'.resume()
 	augroup END
 ]], false)
 
--- vim.api.nvim_exec([[
--- 	augroup toggle_statusline
--- 		autocmd!
--- 		autocmd VimResume,FocusGained * lua resume()
--- 	augroup END
--- ]], false)
 
 
