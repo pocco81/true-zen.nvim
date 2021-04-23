ae7bde51fa0eb9c2a98e33413ee90325de514307
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-23 15:29:33 -0500

    testing resume 4

--- lua/true-zen/services/bottom/init.lua
+++ lua/true-zen/services/bottom/init.lua
@@ -28,8 +28,10 @@ end
 function resume()
 
 	if (bottom_show == 1) then				-- bottm true; shown
+		cmd("echo 'I WAS ONE'")
 		bottom_true()
 	elseif (bottom_show == 0) then			-- status line false; hidden
+		cmd("echo 'I WAS TWO'")
 		bottom_false()
 	elseif (bottom_show == nil) then			-- show var is nil
 		bottom_show = vim.api.nvim_eval("&laststatus > 0")
