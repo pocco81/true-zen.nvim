5d1e472259e38629a0f670a0778e60607e612acc
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-26 14:31:33 -0400

    nonmodifiable buffer fix is unrelated to issue

--- lua/true-zen/services/resume/init.lua
+++ lua/true-zen/services/resume/init.lua
@@ -4,20 +4,12 @@
 
 
 -- might be translated into vimL in the future for better syntax
--- vim.api.nvim_exec([[
--- 	augroup toggle_statusline
--- 		autocmd!
--- 		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.top'.resume()" | endif
--- 		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.bottom'.resume()" | endif
--- 		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.left'.resume()" | endif
--- 	augroup END
--- ]], false)
-
 vim.api.nvim_exec([[
 	augroup toggle_statusline
 		autocmd!
-		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * lua require'true-zen.services.top'.resume()
-		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * lua require'true-zen.services.bottom'.resume()
-		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * lua require'true-zen.services.left'.resume()
+		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.top'.resume()" | endif
+		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.bottom'.resume()" | endif
+		autocmd VimResume,FocusGained,WinEnter,BufWinEnter * if (&modifiable == 1) | execute "lua require'true-zen.services.left'.resume()" | endif
 	augroup END
 ]], false)
+
