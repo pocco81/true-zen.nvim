0a3d1284d958f80b3464005481e953ad3c031629
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-27 09:55:09 -0500

    testing background fix 7

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -377,34 +377,38 @@ function ataraxis_false()		-- hide
 	-- won't work because calling outside of script context
 	vim.api.nvim_exec([[
 
+		function! g:get_color(group, attr)
+			return synIDattr(synIDtrans(hlID(a:group)), a:attr)
+		endfunction
 
-		function! s:set_color(group, attr, color)
+		function! g:set_color(group, attr, color)
 			let gui = has('gui_running') || has('termguicolors') && &termguicolors
 			execute printf('hi %s %s%s=%s', a:group, gui ? 'gui' : 'cterm', a:attr, a:color)
 		endfunction
 
 
 
-		function! s:tranquilize()
-			" let bg = s:get_color('Normal', 'bg#')
+		function! g:tranquilize()
+			let bg = g:get_color('Normal', 'bg#')
 			for grp in ['NonText', 'FoldColumn', 'ColorColumn', 'VertSplit',
 					\ 'StatusLine', 'StatusLineNC', 'SignColumn']
+				" -1 on Vim / '' on GVim
 				if bg == -1 || empty(bg)
-					call s:set_color(grp, 'fg', 'black')
-					call s:set_color(grp, 'bg', 'NONE')
+					call g:set_color(grp, 'fg', 'black')
+					call g:set_color(grp, 'bg', 'NONE')
 				else
-					call s:set_color(grp, 'fg', bg)
-					call s:set_color(grp, 'bg', bg)
+					call g:set_color(grp, 'fg', bg)
+					call g:set_color(grp, 'bg', bg)
 				endif
 
-				call s:set_color(grp, '', 'NONE')
+				call g:set_color(grp, '', 'NONE')
 			endfor
 		endfunction
 
 
-	-- 	call s:tranquilize()
+		call g:tranquilize()
 
-	-- ]], false)
+	]], false)
 
 
 
