83f63bb0dcf28b8d14665848451d1c740cf38c50
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-27 09:33:36 -0500

    testing background fix 4

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -372,39 +372,41 @@ function ataraxis_false()		-- hide
 	cmd([[call BufDo("lua require'true-zen.services.left.init'.main(2)")]])
 
 
-	cmd("source tranq_bkg.vim")
-	-- vim.api.nvim_exec([[
-	-- 	function! s:get_color(group, attr)
-	-- 		return synIDattr(synIDtrans(hlID(a:group)), a:attr)
-	-- 	endfunction
+	-- cmd("source tranq_bkg.vim")
+	vim.api.nvim_exec([[
 
-	-- 	function! s:set_color(group, attr, color)
-	-- 		let gui = has('gui_running') || has('termguicolors') && &termguicolors
-	-- 		execute printf('hi %s %s%s=%s', a:group, gui ? 'gui' : 'cterm', a:attr, a:color)
-	-- 	endfunction
+		function! s:get_color(group, attr)
+			return synIDattr(synIDtrans(hlID(a:group)), a:attr)
+		endfunction
 
-
-
-	-- 	function! s:tranquilize()
-	-- 		let bg = s:get_color('Normal', 'bg#')
-	-- 		for grp in ['NonText', 'FoldColumn', 'ColorColumn', 'VertSplit',
-	-- 				\ 'StatusLine', 'StatusLineNC', 'SignColumn']
-	-- 			" -1 on Vim / '' on GVim
-	-- 			if bg == -1 || empty(bg)
-	-- 				call s:set_color(grp, 'fg', get(g:, 'goyo_bg', 'black'))
-	-- 				call s:set_color(grp, 'bg', 'NONE')
-	-- 			else
-	-- 				call s:set_color(grp, 'fg', bg)
-	-- 				call s:set_color(grp, 'bg', bg)
-	-- 			endif
-
-	-- 			call s:set_color(grp, '', 'NONE')
-	-- 		endfor
-	-- 	endfunction
+		function! s:set_color(group, attr, color)
+			let gui = has('gui_running') || has('termguicolors') && &termguicolors
+			execute printf('hi %s %s%s=%s', a:group, gui ? 'gui' : 'cterm', a:attr, a:color)
+		endfunction
 
 
-	-- 	call s:tranquilize()
-	-- ]], false)
+
+		function! s:tranquilize()
+			let bg = s:get_color('Normal', 'bg#')
+			for grp in ['NonText', 'FoldColumn', 'ColorColumn', 'VertSplit',
+					\ 'StatusLine', 'StatusLineNC', 'SignColumn']
+				" -1 on Vim / '' on GVim
+				if bg == -1 || empty(bg)
+					call s:set_color(grp, 'fg', 'black')
+					call s:set_color(grp, 'bg', 'NONE')
+				else
+					call s:set_color(grp, 'fg', bg)
+					call s:set_color(grp, 'bg', bg)
+				endif
+
+				call s:set_color(grp, '', 'NONE')
+			endfor
+		endfunction
+
+
+		call s:tranquilize()
+
+	]], false)
 
 
 
