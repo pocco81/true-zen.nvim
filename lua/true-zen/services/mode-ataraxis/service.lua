12d0246bc18ecf9ad626a00afb759056ef8801c1
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-27 09:52:30 -0500

    testing background fix 6

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -372,14 +372,11 @@ function ataraxis_false()		-- hide
 	cmd([[call BufDo("lua require'true-zen.services.left.init'.main(2)")]])
 
 
-	cmd("source tranq_bkg.vim")
+	-- cmd("source tranq_bkg.vim")
 	
 	-- won't work because calling outside of script context
 	vim.api.nvim_exec([[
 
-		" function! s:get_color(group, attr)
-		"	return synIDattr(synIDtrans(hlID(a:group)), a:attr)
-		" endfunction
 
 		function! s:set_color(group, attr, color)
 			let gui = has('gui_running') || has('termguicolors') && &termguicolors
@@ -392,7 +389,6 @@ function ataraxis_false()		-- hide
 			" let bg = s:get_color('Normal', 'bg#')
 			for grp in ['NonText', 'FoldColumn', 'ColorColumn', 'VertSplit',
 					\ 'StatusLine', 'StatusLineNC', 'SignColumn']
-				" -1 on Vim / '' on GVim
 				if bg == -1 || empty(bg)
 					call s:set_color(grp, 'fg', 'black')
 					call s:set_color(grp, 'bg', 'NONE')
