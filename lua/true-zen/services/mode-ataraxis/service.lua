cf3e1440157f8d381477dceb13e358ed6a892235
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-29 22:06:20 -0500

    test restoring hi groups 19

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -40,12 +40,101 @@ local function fillchars()
 	cmd([[set fillchars+=eob:\ ]])
 end
 
+local function store_hi_groups()
+
+	vim.api.nvim_exec([[
+		function! ReturnHighlightTerm(group, term)
+			" Store output of group to variable
+			let output = execute('hi ' . a:group)
+
+			" Find the term we're looking for
+			return matchstr(output, a:term.'=\zs\S*')
+		endfunction
+	]], false)
+
+	hi_groups = {
+		NonText = {},
+		FoldColumn = {},
+		ColorColumn= {},
+		VertSplit = {},
+		StatusLine = {},
+		StatusLineNC = {},
+		SignColumn = {}
+	}
+
+	-- term != terminal; term = terminology
+	terms = {
+		'cterm',
+		'ctermbg',
+		'ctermfg',
+		'guibg',
+		'guifg',
+		'gui'
+	}
+
+
+	for hi_index, _ in pairs(hi_groups) do
+
+		for term_index, _ in pairs(terms) do
+			-- local to_call = "[[call ReturnHighlightTerm('"..hi_value.."', '"..term_value.."')]]"
+			cmd("let term_val = ReturnHighlightTerm('"..hi_index.."', '"..terms[term_index].."')")
+			local term_val = vim.api.nvim_eval("g:term_val")
+			-- cmd("echo 'Val = "..term_val.."'")
+
+			if (term_val == "" or term_val == '') then
+				term_val = "NONE"
+			end
+
+			table.insert(hi_groups[hi_index], term_val)
+		end
+
+	end
+
+	hi_groups_stored = true
+	
+end
+
+
+local function restore_hi_groups()
+
+	if (hi_groups_stored == false or hi_groups_stored == nil) then
+
+	elseif (hi_groups_stored == true) then
+		-- cmd("echo '<Space>'")
+		for hi_index, _ in pairs(hi_groups) do
+			-- cmd("echo 'Index = "..tostring(hi_index).."; Value = "..tostring(hi_value).."'")
+			local final_cmd = "highlight "..tostring(hi_index)..""
+			local list_of_terms = ""
+			-- cmd("highlight StatusLine ctermfg=bg ctermbg=bg guibg=bg guifg=bg")
+			for inner_hi_index, _ in pairs(hi_groups[hi_index]) do
+				-- cmd("echo 'Index = "..tostring(inner_hi_index).."; Value = "..tostring(hi_groups[hi_index][inner_hi_index]).."'")
+			
+				-- we need to construct the cmd like so:
+				current_term = terms[inner_hi_index]
+				list_of_terms = list_of_terms.." "..current_term.."="..tostring(hi_groups[hi_index][inner_hi_index])..""
+
+			end
+
+			final_cmd = final_cmd..list_of_terms
+			cmd(final_cmd)
+			-- cmd("echo ' '")
+			-- cmd("echo ' '")
+			-- cmd("echo ' '")
+			-- cmd("echo ' '")
+			-- cmd("echo 'Final cmd = "..final_cmd.."'")
+		end
+
+	end
+	
+end
+
+
 function ataraxis_true()		-- show
 
 	local amount_wins = vim.api.nvim_eval("winnr('$')")
 
 	if (amount_wins == 1) then
-		cmd("echo 'Can not exit Ataraxi Mode because you are currently not in it'")
+		cmd("echo 'TrueZen: can not exit Ataraxi Mode because you are currently not in it'")
 	elseif (amount_wins > 1) then
 		cmd("wincmd h")
 		cmd("q")
@@ -172,127 +261,12 @@ function ataraxis_true()		-- show
 		cmd("set nosplitright")
 	end
 
+
+	restore_hi_groups()
+
 end
 
 
-local function save_hi_groups()
-
-	vim.api.nvim_exec([[
-		function! ReturnHighlightTerm(group, term)
-			" Store output of group to variable
-			let output = execute('hi ' . a:group)
-
-			" Find the term we're looking for
-			return matchstr(output, a:term.'=\zs\S*')
-		endfunction
-	]], false)
-
-	hi_groups = {
-		NonText = {},
-		FoldColumn = {},
-		ColorColumn= {},
-		VertSplit = {},
-		StatusLine = {},
-		StatusLineNC = {},
-		SignColumn = {}
-	}
-
-	-- term != terminal; term = terminology
-	terms = {
-		'cterm',
-		'ctermbg',
-		'ctermfg',
-		'guibg',
-		'guifg',
-		'gui'
-	}
-
-
-	for hi_index, _ in pairs(hi_groups) do
-
-		for term_index, _ in pairs(terms) do
-			-- local to_call = "[[call ReturnHighlightTerm('"..hi_value.."', '"..term_value.."')]]"
-			cmd("let term_val = ReturnHighlightTerm('"..hi_index.."', '"..terms[term_index].."')")
-			local term_val = vim.api.nvim_eval("g:term_val")
-			-- cmd([[call BufDo("set fillchars+=vert:\\ ")]])
-			cmd("echo 'Val = "..term_val.."'")
-
-			if (term_val == "" or term_val == '') then
-				term_val = "NONE"
-			end
-
-			table.insert(hi_groups[hi_index], term_val)
-		end
-
-	end
-
-	hi_groups_stored = true
-
-	-- restore_hi_groups()
-
-	if (hi_groups_stored == false or hi_groups_stored == nil) then
-
-	elseif (hi_groups_stored == true) then
-		cmd("echo '<Space>'")
-		for hi_index, _ in pairs(hi_groups) do
-			-- cmd("echo 'Index = "..tostring(hi_index).."; Value = "..tostring(hi_value).."'")
-			local final_cmd = "highlight "..tostring(hi_index)..""
-			local list_of_terms = ""
-			-- cmd("highlight StatusLine ctermfg=bg ctermbg=bg guibg=bg guifg=bg")
-			for inner_hi_index, _ in pairs(hi_groups[hi_index]) do
-				-- cmd("echo 'Index = "..tostring(inner_hi_index).."; Value = "..tostring(hi_groups[hi_index][inner_hi_index]).."'")
-			
-				-- we need to construct the cmd like so:
-				current_term = terms[inner_hi_index]
-				list_of_terms = list_of_terms.." "..current_term.."="..tostring(hi_groups[hi_index][inner_hi_index])..""
-
-			end
-
-			final_cmd = final_cmd..list_of_terms
-			cmd("echo ' '")
-			cmd("echo ' '")
-			cmd("echo ' '")
-			cmd("echo ' '")
-			cmd("echo 'Final cmd = "..final_cmd.."'")
-		end
-
-	end
-
-	
-end
-
-
--- local function restore_hi_groups()
-
--- 	if (hi_groups_stored == false or hi_groups_stored == nil) then
-
--- 	elseif (hi_groups_stored == true) then
--- 		cmd("echo '<Space>'")
--- 		for hi_index, _ in pairs(hi_groups) do
--- 			-- cmd("echo 'Index = "..tostring(hi_index).."; Value = "..tostring(hi_value).."'")
--- 			local final_cmd = "highlight "..tostring(hi_index)..""
--- 			local list_of_terms = ""
--- 			-- cmd("highlight StatusLine ctermfg=bg ctermbg=bg guibg=bg guifg=bg")
--- 			for inner_hi_index, _ in pairs(hi_groups[hi_index]) do
--- 				-- cmd("echo 'Index = "..tostring(inner_hi_index).."; Value = "..tostring(hi_groups[hi_index][inner_hi_index]).."'")
-			
--- 				-- we need to construct the cmd like so:
--- 				current_term = terms[inner_hi_index]
--- 				list_of_terms = list_of_terms.." "..current_term.."="..tostring(hi_groups[hi_index][inner_hi_index])..""
-
--- 			end
-
--- 			final_cmd = final_cmd..list_of_terms
--- 			cmd("echo ' '")
--- 			cmd("echo ' '")
--- 			cmd("echo ' '")
--- 			cmd("echo ' '")
--- 			cmd("echo 'Final cmd = "..final_cmd.."'")
--- 		end
-
--- 	end
-	
--- end
 
 
 function ataraxis_false()		-- hide
@@ -500,6 +474,8 @@ function ataraxis_false()		-- hide
 
 	if (opts["ataraxis"]["disable_bg_configuration"] == false) then
 		
+		store_hi_groups()
+
 		if (opts["ataraxis"]["custome_bg"] == "" or opts["ataraxis"]["custome_bg"] == '' or opts["ataraxis"]["custome_bg"] == nil) then
 
 			save_hi_groups()
