3e8cd3d101873756d4dd5d2eb3495307a7c2d52a
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-29 21:08:18 -0500

    test restoring hi groups 7

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -226,11 +226,11 @@ local function save_hi_groups()
 
 	elseif (hi_groups_stored == true) then
 		cmd("echo '<Space>'")
-		for hi_index, hi_value in pairs(hi_groups) do
-			cmd("echo 'Index = "..tostring(hi_index).."; Value = "..tostring(hi_value).."'")
-			-- for inner_hi_index, inner_hi_value in pairs(hi_groups[hi_index]) do
-			-- 	cmd("echo 'Index = "..tostring(inner_hi_index).."; Value = "..tostring(inner_hi_value).."'")
-			-- end
+		for hi_index, _ in pairs(hi_groups) do
+			-- cmd("echo 'Index = "..tostring(hi_index).."; Value = "..tostring(hi_value).."'")
+			for inner_hi_index, _ in pairs(hi_groups[hi_index]) do
+				cmd("echo 'Index = "..tostring(inner_hi_index).."; Value = "..tostring(hi_groups[inner_hi_index]).."'")
+			end
 		end
 
 	end
