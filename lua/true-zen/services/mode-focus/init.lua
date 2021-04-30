9ebc303816d617ede325aa70664653d2ff918a70
kdav5758 <sebastiancadavid5758@gmail.com>
2021-04-30 10:23:22 -0500

    fixed execution order 1

--- lua/true-zen/services/mode-focus/init.lua
+++ lua/true-zen/services/mode-focus/init.lua
@@ -65,6 +65,7 @@ local function focus_false()		-- unfocus window
 
 			local ataraxis_is_toggled = require("true-zen.services.mode-ataraxis.init").ataraxis_show
 			cmd("echo 'Ataraxis = "..ataraxis_is_toggled.."'")
+			-- test
 	
 			if (ataraxis_is_toggled == 1) then
 				require'true-zen.main'.main(4, 1)
