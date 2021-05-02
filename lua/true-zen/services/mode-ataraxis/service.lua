64935693effdf9484b75f44ba0ff958581a214f5
kdav5758 <sebastiancadavid5758@gmail.com>
2021-05-02 12:00:43 -0500

    testing hiding and showing methods for galaxyline 2

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -249,36 +249,7 @@ end
 
 
 
-
-function ataraxis_false()		-- hide
-
-	local amount_wins = vim.api.nvim_eval("winnr('$')")
-
-	if (amount_wins > 1) then
-		if (opts["ataraxis"]["force_when_plus_one_window"] == false) then
-			cmd("echo 'TrueZen: TZAtaraxis can not be toggled if there is more than one window open. However, you can force it with the force_when_plus_one_window setting'")
-			goto there_was_more_than_one_window
-		elseif (opts["ataraxis"]["force_when_plus_one_window"] == true) then
-			cmd("only")
-		end
-	else
-		-- nothing
-
-	end
-
-
-	---------------- solves: Vim(Buffer): E86: Buffer 3 does not exist
-	is_splitbelow_set = vim.api.nvim_eval("&splitbelow")
-	is_splitright_set = vim.api.nvim_eval("&splitright")
-
-	if (is_splitbelow_set == 0 or is_splitright_set == 0) then
-		cmd("set splitbelow")
-		cmd("set splitright")
-	else
-		-- continue
-	end
-	---------------- solves: Vim(Buffer): E86: Buffer 3 does not exist
-
+local function load_integrations()
 
 	--------------------------=== Integrations ===--------------------------
 	for opt, _ in pairs(opts["integrations"]) do
@@ -406,6 +377,170 @@ function ataraxis_false()		-- hide
 		end
 	end
 	--------------------------=== Integrations ===--------------------------
+
+	
+end
+
+
+
+function ataraxis_false()		-- hide
+
+	local amount_wins = vim.api.nvim_eval("winnr('$')")
+
+	if (amount_wins > 1) then
+		if (opts["ataraxis"]["force_when_plus_one_window"] == false) then
+			cmd("echo 'TrueZen: TZAtaraxis can not be toggled if there is more than one window open. However, you can force it with the force_when_plus_one_window setting'")
+			goto there_was_more_than_one_window
+		elseif (opts["ataraxis"]["force_when_plus_one_window"] == true) then
+			cmd("only")
+		end
+	else
+		-- nothing
+
+	end
+
+
+	---------------- solves: Vim(Buffer): E86: Buffer 3 does not exist
+	is_splitbelow_set = vim.api.nvim_eval("&splitbelow")
+	is_splitright_set = vim.api.nvim_eval("&splitright")
+
+	if (is_splitbelow_set == 0 or is_splitright_set == 0) then
+		cmd("set splitbelow")
+		cmd("set splitright")
+	else
+		-- continue
+	end
+	---------------- solves: Vim(Buffer): E86: Buffer 3 does not exist
+
+
+	load_integrations()
+
+	----------------------------=== Integrations ===--------------------------
+	--for opt, _ in pairs(opts["integrations"]) do
+	--	if (opts["integrations"][opt] == true) then
+	--		if (opt == "integration_galaxyline") then
+
+	--			require("true-zen.services.bottom.integrations.integration_galaxyline").disable_element()
+
+	--			has_statusline_with_integration = true
+
+	--		elseif (opt == "integration_gitgutter") then
+
+	--			local is_gitgutter_running = vim.api.nvim_eval("get(g:, 'gitgutter_enabled', 0)")
+
+	--			if (is_gitgutter_running == 1) then		-- is running
+	--				require("true-zen.services.bottom.integrations.integration_gitgutter").disable_element()
+	--			elseif (is_gitgutter_running == 0) then		-- is not running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
+
+	--		elseif (opt == "integration_vim_signify") then
+
+	--			local is_vim_signify_running = vim.api.nvim_eval("empty(getbufvar(bufnr(''), 'sy'))")
+
+	--			if (is_vim_signify_running == 1) then		-- is running
+	--				require("true-zen.services.bottom.integrations.integration_vim_signify").disable_element()
+	--			elseif (is_vim_signify_running == 0) then		-- is not running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
+
+	--		elseif (opt == "integration_tmux") then
+
+	--			local is_tmux_running = vim.api.nvim_eval("$TMUX")
+
+	--			if (is_tmux_running ~= "") then
+	--				require("true-zen.services.bottom.integrations.integration_tmux").disable_element()
+	--			else
+	--				-- tmux wasn't running
+	--			end
+
+
+	--		elseif (opt == "integration_vim_airline") then
+
+	--			local is_vim_airline_running = vim.api.nvim_eval("exists('#airline')")
+
+	--			if (is_vim_airline_running == 1) then		-- is running
+	--				require("true-zen.services.bottom.integrations.integration_vim_airline").disable_element()
+	--			elseif (is_vim_airline_running == 0) then		-- is not running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
+
+	--			has_statusline_with_integration = true
+
+	--		elseif (opt == "integration_vim_powerline") then
+
+	--			local is_vim_powerline_running = vim.api.nvim_eval("exists('#PowerlineMain')")
+
+	--			if (is_vim_powerline_running == 1) then		-- is running
+	--				require("true-zen.services.bottom.integrations.integration_vim_powerline").disable_element()
+	--			elseif (is_vim_powerline_running == 0) then		-- is not running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
+
+	--			has_statusline_with_integration = true
+
+
+	--		elseif (opt == "integration_express_line") then
+
+	--			require("true-zen.services.bottom.integrations.integration_express_line").disable_element()
+
+	--			has_statusline_with_integration = true
+
+	--		elseif (opt == "integration_limelight") then
+
+	--			require("true-zen.services.bottom.integrations.integration_limelight").enable_element()
+
+	--			has_statusline_with_integration = true
+	
+	--		elseif (opt == "integration_gitsigns") then
+	--			local gs_integration = require("true-zen.services.bottom.integrations.integration_gitsigns")
+	--			local gs_config = require("gitsigns")._get_config()
+
+	--			gs_ps_current_line_blame = nil
+	--			gs_ps_numhl = nil
+	--			gs_ps_linehl = nil
+	--			gs_ps_signs = nil
+
+	--			if (gs_config.current_line_blame == true) then
+	--				gs_integration.toggle_element(0)
+	--			else
+	--				gs_ps_current_line_blame = false
+	--			end
+				
+	--			if (gs_config.numhl == true) then
+	--				gs_integration.toggle_element(1)
+	--			else
+	--				gs_ps_numhl = false
+	--			end
+				
+	--			if (gs_config.linehl == true) then
+	--				gs_integration.toggle_element(2)
+	--			else
+	--				gs_ps_linehl = false
+	--			end
+
+	--			if (gs_config.signs == true) then
+	--				gs_integration.toggle_element(3)
+	--			else
+	--				gs_ps_signs = false
+	--			end
+
+	--		else
+	--			-- integration not recognized
+	--		end
+	--	else
+	--		-- ignore it
+	--	end
+	--end
+	----------------------------=== Integrations ===--------------------------
 
 
 	local left_padding_cmd = ""
