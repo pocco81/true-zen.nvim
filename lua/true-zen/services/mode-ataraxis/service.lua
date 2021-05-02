93d98975037c4446a23411da62622b69fba97f3b
kdav5758 <sebastiancadavid5758@gmail.com>
2021-05-02 12:19:10 -0500

    solving resume fix 0

--- lua/true-zen/services/mode-ataraxis/service.lua
+++ lua/true-zen/services/mode-ataraxis/service.lua
@@ -108,7 +108,7 @@ function ataraxis_true()		-- show
 	--------------------------=== Hi Groups ===--------------------------
 
 
-	if (has_statusline_with_integration == true) then
+	if (integration.has_statusline_with_integration == true) then
 		-- ignore
 	else
 		cmd("setlocal statusline="..current_statusline.."")
@@ -116,271 +116,272 @@ function ataraxis_true()		-- show
 
 
 
-	--------------------------=== Integrations ===--------------------------
-	for opt, _ in pairs(opts["integrations"]) do
-		if (opts["integrations"][opt] == true) then
-			if (opt == "integration_galaxyline") then
-				require("true-zen.services.bottom.integrations.integration_galaxyline").enable_element()
-				has_statusline_with_integration = true
-			elseif (opt == "integration_gitgutter") then
+	integration.load_integrations(true)
+	----------------------------=== Integrations ===--------------------------
+	--for opt, _ in pairs(opts["integrations"]) do
+	--	if (opts["integrations"][opt] == true) then
+	--		if (opt == "integration_galaxyline") then
+	--			require("true-zen.services.bottom.integrations.integration_galaxyline").enable_element()
+	--			has_statusline_with_integration = true
+	--		elseif (opt == "integration_gitgutter") then
 
-				local is_gitgutter_running = vim.api.nvim_eval("get(g:, 'gitgutter_enabled', 0)")
+	--			local is_gitgutter_running = vim.api.nvim_eval("get(g:, 'gitgutter_enabled', 0)")
 
-				if (is_gitgutter_running == 0) then		-- is not running
-					require("true-zen.services.bottom.integrations.integration_gitgutter").enable_element()
-				elseif (is_gitgutter_running == 1) then		-- is not running
-					-- nothing
-				else
-					-- nothing either
-				end
+	--			if (is_gitgutter_running == 0) then		-- is not running
+	--				require("true-zen.services.bottom.integrations.integration_gitgutter").enable_element()
+	--			elseif (is_gitgutter_running == 1) then		-- is not running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
 
-			elseif (opt == "integration_vim_signify") then
+	--		elseif (opt == "integration_vim_signify") then
 
 
-				local is_vim_signify_running = vim.api.nvim_eval("empty(getbufvar(bufnr(''), 'sy'))")
+	--			local is_vim_signify_running = vim.api.nvim_eval("empty(getbufvar(bufnr(''), 'sy'))")
 
-				if (is_vim_signify_running == 0) then		-- is not running
-					require("true-zen.services.bottom.integrations.integration_vim_signify").enable_element()
-				elseif (is_vim_signify_running == 1) then		-- is running
-					-- nothing
-				else
-					-- nothing either
-				end
+	--			if (is_vim_signify_running == 0) then		-- is not running
+	--				require("true-zen.services.bottom.integrations.integration_vim_signify").enable_element()
+	--			elseif (is_vim_signify_running == 1) then		-- is running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
 
-			elseif (opt == "integration_tmux") then
+	--		elseif (opt == "integration_tmux") then
 			
-				local is_tmux_running = vim.api.nvim_eval("$TMUX")
+	--			local is_tmux_running = vim.api.nvim_eval("$TMUX")
 
-				if (is_tmux_running ~= "") then		-- is running
-					require("true-zen.services.bottom.integrations.integration_tmux").enable_element()
-				else
-					-- tmux wasn't running
-				end
+	--			if (is_tmux_running ~= "") then		-- is running
+	--				require("true-zen.services.bottom.integrations.integration_tmux").enable_element()
+	--			else
+	--				-- tmux wasn't running
+	--			end
 
-			elseif (opt == "integration_vim_airline") then
-
-
-				local is_vim_airline_running = vim.api.nvim_eval("exists('#airline')")
-
-				if (is_vim_airline_running == 0) then		-- is not running
-					require("true-zen.services.bottom.integrations.integration_vim_airline").enable_element()
-				elseif (is_vim_airline_running == 1) then		-- is running
-					-- nothing
-				else
-					-- nothing either
-				end
-
-				has_statusline_with_integration = true
-
-			elseif (opt == "integration_vim_powerline") then
+	--		elseif (opt == "integration_vim_airline") then
 
 
-				local is_vim_powerline_running = vim.api.nvim_eval("exists('#PowerlineMain')")
+	--			local is_vim_airline_running = vim.api.nvim_eval("exists('#airline')")
 
-				if (is_vim_powerline_running == 0) then		-- is not running
-					require("true-zen.services.bottom.integrations.integration_vim_powerline").enable_element()
-				elseif (is_vim_powerline_running == 1) then		-- is running
-					-- nothing
-				else
-					-- nothing either
-				end
+	--			if (is_vim_airline_running == 0) then		-- is not running
+	--				require("true-zen.services.bottom.integrations.integration_vim_airline").enable_element()
+	--			elseif (is_vim_airline_running == 1) then		-- is running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
 
-				has_statusline_with_integration = true
+	--			has_statusline_with_integration = true
 
-			elseif (opt == "integration_express_line") then
-
-				require("true-zen.services.bottom.integrations.integration_express_line").enable_element()
-
-				has_statusline_with_integration = true
-
-			elseif (opt == "integration_limelight") then
-
-				require("true-zen.services.bottom.integrations.integration_limelight").disable_element()
-
-				has_statusline_with_integration = true
-
-			elseif (opt == "integration_gitsigns") then
-
-				local gs_integration = require("true-zen.services.bottom.integrations.integration_gitsigns")
-				local gs_config = require("gitsigns")._get_config()
+	--		elseif (opt == "integration_vim_powerline") then
 
 
-				if (gs_ps_current_line_blame == nil) then
-					gs_integration.toggle_element(0)
-				else
-					if (gs_ps_current_line_blame == false) then
-						-- it's already false
-					end
-				end
+	--			local is_vim_powerline_running = vim.api.nvim_eval("exists('#PowerlineMain')")
 
-				if (gs_ps_numhl == nil) then
-					gs_integration.toggle_element(1)
-				else
-					if (gs_ps_current_line_blame == false) then
-						-- it's already false
-					end
-				end
+	--			if (is_vim_powerline_running == 0) then		-- is not running
+	--				require("true-zen.services.bottom.integrations.integration_vim_powerline").enable_element()
+	--			elseif (is_vim_powerline_running == 1) then		-- is running
+	--				-- nothing
+	--			else
+	--				-- nothing either
+	--			end
 
-				if (gs_ps_linehl == nil) then
-					gs_integration.toggle_element(2)
-				else
-					if (gs_ps_linehl == false) then
-						-- it's already false
-					end
-				end
+	--			has_statusline_with_integration = true
 
-				if (gs_ps_signs == nil) then
-					gs_integration.toggle_element(3)
-				else
-					if (gs_ps_signs == false) then
-						-- it's already false
-					end
-				end
+	--		elseif (opt == "integration_express_line") then
 
-			else
-				-- integration not recognized
-			end
-		else
-			-- ignore it
-		end
-	end
-	--------------------------=== Integrations ===--------------------------
+	--			require("true-zen.services.bottom.integrations.integration_express_line").enable_element()
+
+	--			has_statusline_with_integration = true
+
+	--		elseif (opt == "integration_limelight") then
+
+	--			require("true-zen.services.bottom.integrations.integration_limelight").disable_element()
+
+	--			has_statusline_with_integration = true
+
+	--		elseif (opt == "integration_gitsigns") then
+
+	--			local gs_integration = require("true-zen.services.bottom.integrations.integration_gitsigns")
+	--			local gs_config = require("gitsigns")._get_config()
+
+
+	--			if (gs_ps_current_line_blame == nil) then
+	--				gs_integration.toggle_element(0)
+	--			else
+	--				if (gs_ps_current_line_blame == false) then
+	--					-- it's already false
+	--				end
+	--			end
+
+	--			if (gs_ps_numhl == nil) then
+	--				gs_integration.toggle_element(1)
+	--			else
+	--				if (gs_ps_current_line_blame == false) then
+	--					-- it's already false
+	--				end
+	--			end
+
+	--			if (gs_ps_linehl == nil) then
+	--				gs_integration.toggle_element(2)
+	--			else
+	--				if (gs_ps_linehl == false) then
+	--					-- it's already false
+	--				end
+	--			end
+
+	--			if (gs_ps_signs == nil) then
+	--				gs_integration.toggle_element(3)
+	--			else
+	--				if (gs_ps_signs == false) then
+	--					-- it's already false
+	--				end
+	--			end
+
+	--		else
+	--			-- integration not recognized
+	--		end
+	--	else
+	--		-- ignore it
+	--	end
+	--end
+	----------------------------=== Integrations ===--------------------------
 
 end
 
 
 
-function load_integrations()
+--function load_integrations()
 
-	--------------------------=== Integrations ===--------------------------
-	for opt, _ in pairs(opts["integrations"]) do
-		if (opts["integrations"][opt] == true) then
-			if (opt == "integration_galaxyline") then
+--	--------------------------=== Integrations ===--------------------------
+--	for opt, _ in pairs(opts["integrations"]) do
+--		if (opts["integrations"][opt] == true) then
+--			if (opt == "integration_galaxyline") then
 
-				require("true-zen.services.bottom.integrations.integration_galaxyline").disable_element()
+--				require("true-zen.services.bottom.integrations.integration_galaxyline").disable_element()
 
-				has_statusline_with_integration = true
+--				has_statusline_with_integration = true
 
-			elseif (opt == "integration_gitgutter") then
+--			elseif (opt == "integration_gitgutter") then
 
-				local is_gitgutter_running = vim.api.nvim_eval("get(g:, 'gitgutter_enabled', 0)")
+--				local is_gitgutter_running = vim.api.nvim_eval("get(g:, 'gitgutter_enabled', 0)")
 
-				if (is_gitgutter_running == 1) then		-- is running
-					require("true-zen.services.bottom.integrations.integration_gitgutter").disable_element()
-				elseif (is_gitgutter_running == 0) then		-- is not running
-					-- nothing
-				else
-					-- nothing either
-				end
+--				if (is_gitgutter_running == 1) then		-- is running
+--					require("true-zen.services.bottom.integrations.integration_gitgutter").disable_element()
+--				elseif (is_gitgutter_running == 0) then		-- is not running
+--					-- nothing
+--				else
+--					-- nothing either
+--				end
 
-			elseif (opt == "integration_vim_signify") then
+--			elseif (opt == "integration_vim_signify") then
 
-				local is_vim_signify_running = vim.api.nvim_eval("empty(getbufvar(bufnr(''), 'sy'))")
+--				local is_vim_signify_running = vim.api.nvim_eval("empty(getbufvar(bufnr(''), 'sy'))")
 
-				if (is_vim_signify_running == 1) then		-- is running
-					require("true-zen.services.bottom.integrations.integration_vim_signify").disable_element()
-				elseif (is_vim_signify_running == 0) then		-- is not running
-					-- nothing
-				else
-					-- nothing either
-				end
+--				if (is_vim_signify_running == 1) then		-- is running
+--					require("true-zen.services.bottom.integrations.integration_vim_signify").disable_element()
+--				elseif (is_vim_signify_running == 0) then		-- is not running
+--					-- nothing
+--				else
+--					-- nothing either
+--				end
 
-			elseif (opt == "integration_tmux") then
+--			elseif (opt == "integration_tmux") then
 
-				local is_tmux_running = vim.api.nvim_eval("$TMUX")
+--				local is_tmux_running = vim.api.nvim_eval("$TMUX")
 
-				if (is_tmux_running ~= "") then
-					require("true-zen.services.bottom.integrations.integration_tmux").disable_element()
-				else
-					-- tmux wasn't running
-				end
-
-
-			elseif (opt == "integration_vim_airline") then
-
-				local is_vim_airline_running = vim.api.nvim_eval("exists('#airline')")
-
-				if (is_vim_airline_running == 1) then		-- is running
-					require("true-zen.services.bottom.integrations.integration_vim_airline").disable_element()
-				elseif (is_vim_airline_running == 0) then		-- is not running
-					-- nothing
-				else
-					-- nothing either
-				end
-
-				has_statusline_with_integration = true
-
-			elseif (opt == "integration_vim_powerline") then
-
-				local is_vim_powerline_running = vim.api.nvim_eval("exists('#PowerlineMain')")
-
-				if (is_vim_powerline_running == 1) then		-- is running
-					require("true-zen.services.bottom.integrations.integration_vim_powerline").disable_element()
-				elseif (is_vim_powerline_running == 0) then		-- is not running
-					-- nothing
-				else
-					-- nothing either
-				end
-
-				has_statusline_with_integration = true
+--				if (is_tmux_running ~= "") then
+--					require("true-zen.services.bottom.integrations.integration_tmux").disable_element()
+--				else
+--					-- tmux wasn't running
+--				end
 
 
-			elseif (opt == "integration_express_line") then
+--			elseif (opt == "integration_vim_airline") then
 
-				require("true-zen.services.bottom.integrations.integration_express_line").disable_element()
+--				local is_vim_airline_running = vim.api.nvim_eval("exists('#airline')")
 
-				has_statusline_with_integration = true
+--				if (is_vim_airline_running == 1) then		-- is running
+--					require("true-zen.services.bottom.integrations.integration_vim_airline").disable_element()
+--				elseif (is_vim_airline_running == 0) then		-- is not running
+--					-- nothing
+--				else
+--					-- nothing either
+--				end
 
-			elseif (opt == "integration_limelight") then
+--				has_statusline_with_integration = true
 
-				require("true-zen.services.bottom.integrations.integration_limelight").enable_element()
+--			elseif (opt == "integration_vim_powerline") then
 
-				has_statusline_with_integration = true
+--				local is_vim_powerline_running = vim.api.nvim_eval("exists('#PowerlineMain')")
+
+--				if (is_vim_powerline_running == 1) then		-- is running
+--					require("true-zen.services.bottom.integrations.integration_vim_powerline").disable_element()
+--				elseif (is_vim_powerline_running == 0) then		-- is not running
+--					-- nothing
+--				else
+--					-- nothing either
+--				end
+
+--				has_statusline_with_integration = true
+
+
+--			elseif (opt == "integration_express_line") then
+
+--				require("true-zen.services.bottom.integrations.integration_express_line").disable_element()
+
+--				has_statusline_with_integration = true
+
+--			elseif (opt == "integration_limelight") then
+
+--				require("true-zen.services.bottom.integrations.integration_limelight").enable_element()
+
+--				has_statusline_with_integration = true
 	
-			elseif (opt == "integration_gitsigns") then
-				local gs_integration = require("true-zen.services.bottom.integrations.integration_gitsigns")
-				local gs_config = require("gitsigns")._get_config()
+--			elseif (opt == "integration_gitsigns") then
+--				local gs_integration = require("true-zen.services.bottom.integrations.integration_gitsigns")
+--				local gs_config = require("gitsigns")._get_config()
 
-				gs_ps_current_line_blame = nil
-				gs_ps_numhl = nil
-				gs_ps_linehl = nil
-				gs_ps_signs = nil
+--				gs_ps_current_line_blame = nil
+--				gs_ps_numhl = nil
+--				gs_ps_linehl = nil
+--				gs_ps_signs = nil
 
-				if (gs_config.current_line_blame == true) then
-					gs_integration.toggle_element(0)
-				else
-					gs_ps_current_line_blame = false
-				end
+--				if (gs_config.current_line_blame == true) then
+--					gs_integration.toggle_element(0)
+--				else
+--					gs_ps_current_line_blame = false
+--				end
 				
-				if (gs_config.numhl == true) then
-					gs_integration.toggle_element(1)
-				else
-					gs_ps_numhl = false
-				end
+--				if (gs_config.numhl == true) then
+--					gs_integration.toggle_element(1)
+--				else
+--					gs_ps_numhl = false
+--				end
 				
-				if (gs_config.linehl == true) then
-					gs_integration.toggle_element(2)
-				else
-					gs_ps_linehl = false
-				end
+--				if (gs_config.linehl == true) then
+--					gs_integration.toggle_element(2)
+--				else
+--					gs_ps_linehl = false
+--				end
 
-				if (gs_config.signs == true) then
-					gs_integration.toggle_element(3)
-				else
-					gs_ps_signs = false
-				end
+--				if (gs_config.signs == true) then
+--					gs_integration.toggle_element(3)
+--				else
+--					gs_ps_signs = false
+--				end
 
-			else
-				-- integration not recognized
-			end
-		else
-			-- ignore it
-		end
-	end
-	--------------------------=== Integrations ===--------------------------
+--			else
+--				-- integration not recognized
+--			end
+--		else
+--			-- ignore it
+--		end
+--	end
+--	--------------------------=== Integrations ===--------------------------
 
 	
-end
+--end
 
 
 
