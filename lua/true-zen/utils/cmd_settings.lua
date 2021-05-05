4707c86289ba10d4cc530a5c043e0fa9d884c862
kdav5758 <kdav5758@gmail.com>
2021-05-05 15:51:22 -0500

    testing execution 28

--- lua/true-zen/utils/cmd_settings.lua
+++ lua/true-zen/utils/cmd_settings.lua
@@ -100,34 +100,34 @@ function map_settings(table, bool, ui_element)
 					end
 				end
 				]]--
-			if (ui_element == "TOP") then
-				if (top_has_been_restored == false) then
-					top_has_been_restored = false
-					-- cmd("echo 'here!!! HBR = "..tostring(top_has_been_restored).."'")
-					-- top_only_here = false
-				elseif (top_has_been_restored == true or top_has_been_restored == nil) then
-					if (top_only_here == false) then
-						-- ignore
-					else
-						top_has_been_restored = before_after_cmds.store_settings(table, ui_element)
-						-- cmd("echo '(1) I ran btw HBR = "..tostring(top_has_been_restored).."'")
-					end
-				end
-			elseif (ui_element == "LEFT") then
-				if (left_has_been_restored == false) then
-					left_has_been_restored = false
-					-- cmd("echo 'here!!! HBR = "..tostring(left_has_been_restored).."'")
-					left_only_here = false
-				elseif (left_has_been_restored == true or left_has_been_restored == nil) then
-					if (left_only_here == false) then
-						-- ignore
-					else
-						left_has_been_restored = before_after_cmds.store_settings(table, ui_element)
-						-- cmd("echo '(2) I ran btw HBR = "..tostring(left_has_been_restored).."'")
-					end
-				end
+			-- if (ui_element == "TOP") then
+			-- 	if (top_has_been_restored == false) then
+			-- 		top_has_been_restored = false
+			-- 		-- cmd("echo 'here!!! HBR = "..tostring(top_has_been_restored).."'")
+			-- 		-- top_only_here = false
+			-- 	elseif (top_has_been_restored == true or top_has_been_restored == nil) then
+			-- 		if (top_only_here == false) then
+			-- 			-- ignore
+			-- 		else
+			-- 			top_has_been_restored = before_after_cmds.store_settings(table, ui_element)
+			-- 			-- cmd("echo '(1) I ran btw HBR = "..tostring(top_has_been_restored).."'")
+			-- 		end
+			-- 	end
+			-- elseif (ui_element == "LEFT") then
+			-- 	if (left_has_been_restored == false) then
+			-- 		left_has_been_restored = false
+			-- 		-- cmd("echo 'here!!! HBR = "..tostring(left_has_been_restored).."'")
+			-- 		left_only_here = false
+			-- 	elseif (left_has_been_restored == true or left_has_been_restored == nil) then
+			-- 		if (left_only_here == false) then
+			-- 			-- ignore
+			-- 		else
+			-- 			left_has_been_restored = before_after_cmds.store_settings(table, ui_element)
+			-- 			-- cmd("echo '(2) I ran btw HBR = "..tostring(left_has_been_restored).."'")
+			-- 		end
+			-- 	end
 
-			end
+			-- end
 
 
 
