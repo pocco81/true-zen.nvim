58e424ea1aacbe9ca2bf750f05e4dbd51cc80d65
kdav5758 <kdav5758@gmail.com>
2021-06-17 10:52:17 -0500

    testing var restoration 7

--- lua/true-zen/utils/cmd_settings.lua
+++ lua/true-zen/utils/cmd_settings.lua
@@ -66,8 +66,8 @@ function map_settings(table, bool, ui_element)
     elseif (bool == false) then
         if (opts["minimalist"]["store_and_restore_settings"] == true) then
 			print("I ran false")
-			-- local minimalist_show = require("true-zen.services.mode-minimalist.init").minimalist_show
-			-- print("minimalist_show = "..tostring(minimalist_show))
+			local minimalist_show = require("true-zen.services.mode-minimalist.init").minimalist_show
+			print("minimalist_show = "..tostring(minimalist_show))
 			if (minimalist_show == 1 or minimalist_show == nil) then
 				if (ui_element == "BOTTOM") then
 						before_after_cmds.store_settings(opts["bottom"], "BOTTOM")
