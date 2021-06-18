a45d04a9ce15a2fcada6315a299256f2f2dd6bf7
kdav5758 <kdav5758@gmail.com>
2021-06-18 09:28:39 -0500

    added if

--- lua/true-zen/utils/cmd_settings.lua
+++ lua/true-zen/utils/cmd_settings.lua
@@ -123,13 +123,13 @@ function map_settings(table, bool, ui_element)
                 before_after_cmds.restore_settings(ui_element)
                 analyse_ui_element(table, ui_element, true)
 			else
-                -- if (vim.g.__truezen_ataraxis_hiding == "true") then
+                if (vim.g.__truezen_ataraxis_hiding == "true") then
 					for opt, _ in pairs(table) do
 						if string.find(opt, "shown_") then
 							clean_and_exec(opt, table[opt], "shown_")
 						end
 					end
-				-- end
+				end
             end
         else
             for opt, _ in pairs(table) do
