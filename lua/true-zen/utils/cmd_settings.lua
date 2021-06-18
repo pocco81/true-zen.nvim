cb30b2378ff9366afcea5f24695bd08edfac6e4c
kdav5758 <kdav5758@gmail.com>
2021-06-18 08:59:45 -0500

    added for

--- lua/true-zen/utils/cmd_settings.lua
+++ lua/true-zen/utils/cmd_settings.lua
@@ -34,66 +34,66 @@ local function clean_and_exec(opt, table_opt, remove_str)
 end
 
 local function analyse_ui_element(table, ui_element, state)
-	if (state == true) then
-		if (ui_element == "BOTTOM") then
-			local bottom_has_been_stored = before_after_cmds.get_has_been_stored("BOTTOM")
+    if (state == true) then
+        if (ui_element == "BOTTOM") then
+            local bottom_has_been_stored = before_after_cmds.get_has_been_stored("BOTTOM")
 
-			if (bottom_has_been_stored == false or bottom_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["bottom"], "BOTTOM")
-			end
-		elseif (ui_element == "TOP") then
-			local top_has_been_stored = before_after_cmds.get_has_been_stored("TOP")
+            if (bottom_has_been_stored == false or bottom_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["bottom"], "BOTTOM")
+            end
+        elseif (ui_element == "TOP") then
+            local top_has_been_stored = before_after_cmds.get_has_been_stored("TOP")
 
-			if (top_has_been_stored == true or top_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["top"], "TOP")
-			end
-		elseif (ui_element == "LEFT") then
-			local left_has_been_stored = before_after_cmds.get_has_been_stored("LEFT")
+            if (top_has_been_stored == true or top_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["top"], "TOP")
+            end
+        elseif (ui_element == "LEFT") then
+            local left_has_been_stored = before_after_cmds.get_has_been_stored("LEFT")
 
-			if (left_has_been_stored == true or left_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["left"], "LEFT")
-			end
-		else
-			cmd("echo 'TrueZen: UI Element was not recognized'")
-		end
+            if (left_has_been_stored == true or left_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["left"], "LEFT")
+            end
+        else
+            cmd("echo 'TrueZen: UI Element was not recognized'")
+        end
 
-		before_after_cmds.restore_settings(ui_element)
-		if (#opts["minimalist"]["show_vals_to_read"] > 0) then
-			for opt, _ in pairs(opts["minimalist"]["show_vals_to_read"]) do
-				for inner_opt, _ in pairs(table) do
-					if (tostring(opts["minimalist"]["show_vals_to_read"][opt]) == tostring(inner_opt)) then
-						if string.find(inner_opt, "shown_") then
-							clean_and_exec(inner_opt, table[inner_opt], "shown_")
-						end
-					end
-				end
-			end
-		end
-	else
-		if (ui_element == "BOTTOM") then
-			local bottom_has_been_stored = before_after_cmds.get_has_been_stored("BOTTOM")
-			if (bottom_has_been_stored == false or bottom_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["bottom"], "BOTTOM")
-			end
-		elseif (ui_element == "TOP") then
-			local top_has_been_stored = before_after_cmds.get_has_been_stored("TOP")
-			if (top_has_been_stored == true or top_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["top"], "TOP")
-			end
-		elseif (ui_element == "LEFT") then
-			local left_has_been_stored = before_after_cmds.get_has_been_stored("LEFT")
-			if (left_has_been_stored == true or left_has_been_stored == nil) then
-				before_after_cmds.store_settings(opts["left"], "LEFT")
-			end
-		else
-			cmd("echo 'TrueZen: UI Element was not recognized'")
-		end
-		for opt, _ in pairs(table) do
-			if string.find(opt, "hidden_") then
-				clean_and_exec(opt, table[opt], "hidden_")
-			end
-		end
-	end
+        before_after_cmds.restore_settings(ui_element)
+        if (#opts["minimalist"]["show_vals_to_read"] > 0) then
+            for opt, _ in pairs(opts["minimalist"]["show_vals_to_read"]) do
+                for inner_opt, _ in pairs(table) do
+                    if (tostring(opts["minimalist"]["show_vals_to_read"][opt]) == tostring(inner_opt)) then
+                        if string.find(inner_opt, "shown_") then
+                            clean_and_exec(inner_opt, table[inner_opt], "shown_")
+                        end
+                    end
+                end
+            end
+        end
+    else
+        if (ui_element == "BOTTOM") then
+            local bottom_has_been_stored = before_after_cmds.get_has_been_stored("BOTTOM")
+            if (bottom_has_been_stored == false or bottom_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["bottom"], "BOTTOM")
+            end
+        elseif (ui_element == "TOP") then
+            local top_has_been_stored = before_after_cmds.get_has_been_stored("TOP")
+            if (top_has_been_stored == true or top_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["top"], "TOP")
+            end
+        elseif (ui_element == "LEFT") then
+            local left_has_been_stored = before_after_cmds.get_has_been_stored("LEFT")
+            if (left_has_been_stored == true or left_has_been_stored == nil) then
+                before_after_cmds.store_settings(opts["left"], "LEFT")
+            end
+        else
+            cmd("echo 'TrueZen: UI Element was not recognized'")
+        end
+        for opt, _ in pairs(table) do
+            if string.find(opt, "hidden_") then
+                clean_and_exec(opt, table[opt], "hidden_")
+            end
+        end
+    end
 end
 
 function map_settings(table, bool, ui_element)
@@ -104,25 +104,31 @@ function map_settings(table, bool, ui_element)
             -- print("I ran true")
             -- print("minimalist_show = "..tostring(require("true-zen.services.mode-minimalist.init").get_minimalist_show()))
             print("true")
-			local analyse = false
+            local analyse = false
 
             if (vim.g.__truezen_focus_loaded == "false") then
-				print("Focus says it should not be analyzed")
-				analyse = false
-			end
+                print("Focus says it should not be analyzed")
+                analyse = false
+            end
 
-			if (vim.g.__truezen_minimalist_hiding == "false") then
-				print("Minimalist says it should be analyzed")
-				analyse = true
-			elseif (vim.g.__truezen_minimalist_hiding == "true") then
-				analyse = false
-			end
+            if (vim.g.__truezen_minimalist_hiding == "false") then
+                print("Minimalist says it should be analyzed")
+                analyse = true
+            elseif (vim.g.__truezen_minimalist_hiding == "true") then
+                analyse = false
+            end
 
-			if (analyse == true) then
-				print("RESTORING...")
-				before_after_cmds.restore_settings(ui_element)
-				analyse_ui_element(table, ui_element, true)
-			end
+            if (analyse == true) then
+                print("RESTORING...")
+                before_after_cmds.restore_settings(ui_element)
+                analyse_ui_element(table, ui_element, true)
+			else
+				for opt, _ in pairs(table) do
+					if string.find(opt, "shown_") then
+						clean_and_exec(opt, table[opt], "shown_")
+					end
+				end
+            end
         else
             for opt, _ in pairs(table) do
                 if string.find(opt, "shown_") then
@@ -133,22 +139,21 @@ function map_settings(table, bool, ui_element)
     elseif (bool == false) then
         if (opts["minimalist"]["store_and_restore_settings"] == true) then
             print("false")
-            print("Ataraxis hiding = "..tostring(vim.g.__truezen_minimalist_hiding))
+            print("Ataraxis hiding = " .. tostring(vim.g.__truezen_minimalist_hiding))
 
             -- if (vim.g.__truezen_ataraxis_hiding == "false" or vim.g.__truezen_ataraxis_hiding == nil) then
-			if (vim.g.__truezen_minimalist_hiding == "true" or vim.g.__truezen_minimalist_hiding == nil) then
-				print("I WAS TRUE OR NIL")
-				analyse_ui_element(table, ui_element, false)
-			else
-				print("I WAS FAAAALSE")
-				if (vim.g.__truezen_ataraxis_hiding == "true") then
-					for opt, _ in pairs(table) do
-						if string.find(opt, "hidden_") then
-							clean_and_exec(opt, table[opt], "hidden_")
-						end
-					end
-				end
-
+            if (vim.g.__truezen_minimalist_hiding == "true" or vim.g.__truezen_minimalist_hiding == nil) then
+                print("I WAS TRUE OR NIL")
+                analyse_ui_element(table, ui_element, false)
+            else
+                print("I WAS FAAAALSE")
+                if (vim.g.__truezen_ataraxis_hiding == "true") then
+                    for opt, _ in pairs(table) do
+                        if string.find(opt, "hidden_") then
+                            clean_and_exec(opt, table[opt], "hidden_")
+                        end
+                    end
+                end
             end
         end
     end
