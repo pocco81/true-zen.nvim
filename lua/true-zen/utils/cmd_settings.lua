bcdb669205346597938237fcd827f98e1e53aa0f
kdav5758 <kdav5758@gmail.com>
2021-06-17 19:11:54 -0500

    commented global val

--- lua/true-zen/utils/cmd_settings.lua
+++ lua/true-zen/utils/cmd_settings.lua
@@ -88,7 +88,7 @@ function map_settings(table, bool, ui_element)
     elseif (bool == false) then
         if (opts["minimalist"]["store_and_restore_settings"] == true) then
 			print("false")
-			print("Ataraxis hiding = "..vim.g.__truezen_ataraxis_hiding)
+			-- print("Ataraxis hiding = "..vim.g.__truezen_ataraxis_hiding)
 			local minimalist_show = require("true-zen.services.mode-minimalist.init").get_minimalist_show()
 
 			-- if (vim.g.__truezen_ataraxis_hiding == "false" or vim.g.__truezen_ataraxis_hiding == nil) then
