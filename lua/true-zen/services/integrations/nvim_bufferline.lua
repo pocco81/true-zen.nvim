d9a94a98e268dbc3e55e9789081782411bbc9aaa
kdav5758 <kdav5758@gmail.com>
2021-06-30 11:16:08 -0500

    added prints

--- lua/true-zen/services/integrations/nvim_bufferline.lua
+++ lua/true-zen/services/integrations/nvim_bufferline.lua
@@ -5,8 +5,11 @@ function M.run()
 end
 
 function M.enable_element()
+	print("here0")
 	if (require("bufferline.config").get("options")["always_show_bufferline"] == false) then
+			print("here1")
 		if (vim.api.nvim_eval([[len(filter(range(1, bufnr('$')), 'buflisted(v:val)'))]]) > 1) then
+			print("here2")
 			vim.cmd("set showtabline=2")
 		end
 	end
