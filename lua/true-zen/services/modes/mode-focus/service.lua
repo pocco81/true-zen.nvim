89226c6111ef624d2d3cf654ea8669c5e294ba2a
kdav5758 <kdav5758@gmail.com>
2021-07-03 09:17:09 -0500

    fix #31: TZFocus now preserves view for the experimental focusing method

--- lua/true-zen/services/modes/mode-focus/service.lua
+++ lua/true-zen/services/modes/mode-focus/service.lua
@@ -1,10 +1,15 @@
 local cmd = vim.cmd
+local b = vim.b
+local fn = vim.fn
 
 local M = {}
 
 function M.on(focus_type)
     if (focus_type == "experimental") then
+		b.truezen_mode_focus_buffer_view = fn.winsaveview()
         cmd("tabe %")
+		fn.winrestview(b.truezen_mode_focus_buffer_view)
+		cmd("unlet b:truezen_mode_focus_buffer_view")
     elseif (focus_type == "native") then
         cmd("vert resize | resize")
         cmd("normal! ze")
@@ -13,7 +18,10 @@ end
 
 function M.off(focus_type)
     if (focus_type == "experimental") then
+		b.truezen_mode_focus_buffer_view = fn.winsaveview()
         cmd("q")
+		fn.winrestview(b.truezen_mode_focus_buffer_view)
+		cmd("unlet b:truezen_mode_focus_buffer_view")
     elseif (focus_type == "native") then
         cmd("wincmd =")
         cmd("normal! ze")
