5b4dddb2c337cc5f9c5a85aad7f418d04c948092
kdav5758 <kdav5758@gmail.com>
2021-07-06 16:55:07 -0500

    replaced with setlocal

--- lua/true-zen/services/integrations/lualine.lua
+++ lua/true-zen/services/integrations/lualine.lua
@@ -1,14 +1,13 @@
-local api = vim.api
 local cmd = vim.cmd
 
 local M = {}
 
 function M.enable_element()
-	cmd([["set statusline=%!v:lua.require'lualine'.statusline()]])
+	cmd([["setlocal statusline=%!v:lua.require'lualine'.statusline()]])
 end
 
 function M.disable_element()
-    cmd("set statusline=-")
+    cmd("setlocal statusline=-")
 end
 
 return M
