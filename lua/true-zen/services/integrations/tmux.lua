8c3b7de824b89f751b55ead3f3ddc29650c1d424
Pocco81 <pocco451@gmail.com>
2021-08-05 10:27:36 -0500

    fix: set tmux statusline on/off only for current session

--- lua/true-zen/services/integrations/tmux.lua
+++ lua/true-zen/services/integrations/tmux.lua
@@ -3,11 +3,11 @@ local cmd = vim.cmd
 local M = {}
 
 function M.enable_element()
-    cmd("silent !tmux set -g status on")
+    cmd("silent !tmux set status on")
 end
 
 function M.disable_element()
-    cmd("silent !tmux set -g status off")
+    cmd("silent !tmux set status off")
 end
 
 return M
