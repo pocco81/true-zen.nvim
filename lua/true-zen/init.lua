3123185706f0c10fb2796ac594378dd9f935d137
Pocco81 <pocco451@gmail.com>
2021-09-07 21:48:48 -0500

    dev: formatted root

--- lua/true-zen/init.lua
+++ lua/true-zen/init.lua
@@ -3,44 +3,44 @@ local cmd = vim.cmd
 local M = {}
 
 local function setup_commands(opts)
-    if (opts["misc"]["on_off_commands"] == true) then
-        cmd([[command! TZMinimalistOn lua require'true-zen.main'.main(3, 'on')]])
-        cmd([[command! TZMinimalistOff lua require'true-zen.main'.main(3, 'off')]])
-        cmd([[command! TZAtaraxisOn lua require'true-zen.main'.main(4, 'on')]])
-        cmd([[command! TZAtaraxisOff lua require'true-zen.main'.main(4, 'off')]])
-        cmd([[command! TZFocusOn lua require'true-zen.main'.main(5, 'on')]])
-        cmd([[command! TZFocusOff lua require'true-zen.main'.main(5, 'off')]])
+	if opts["misc"]["on_off_commands"] == true then
+		cmd([[command! TZMinimalistOn lua require'true-zen.main'.main(3, 'on')]])
+		cmd([[command! TZMinimalistOff lua require'true-zen.main'.main(3, 'off')]])
+		cmd([[command! TZAtaraxisOn lua require'true-zen.main'.main(4, 'on')]])
+		cmd([[command! TZAtaraxisOff lua require'true-zen.main'.main(4, 'off')]])
+		cmd([[command! TZFocusOn lua require'true-zen.main'.main(5, 'on')]])
+		cmd([[command! TZFocusOff lua require'true-zen.main'.main(5, 'off')]])
 
-        if (opts["misc"]["ui_elements_commands"] == true) then
-            cmd([[command! TZTopOn lua require'true-zen.main'.main(1, 'on')]])
-            cmd([[command! TZTopOff lua require'true-zen.main'.main(1, 'off')]])
-            cmd([[command! TZLeftOn lua require'true-zen.main'.main(2, 'on')]])
-            cmd([[command! TZLeftOff lua require'true-zen.main'.main(2, 'off')]])
-            cmd([[command! TZBottomOn lua require'true-zen.main'.main(0, 'on')]])
-            cmd([[command! TZBottomOff lua require'true-zen.main'.main(0, 'off')]])
-        end
-    end
+		if opts["misc"]["ui_elements_commands"] == true then
+			cmd([[command! TZTopOn lua require'true-zen.main'.main(1, 'on')]])
+			cmd([[command! TZTopOff lua require'true-zen.main'.main(1, 'off')]])
+			cmd([[command! TZLeftOn lua require'true-zen.main'.main(2, 'on')]])
+			cmd([[command! TZLeftOff lua require'true-zen.main'.main(2, 'off')]])
+			cmd([[command! TZBottomOn lua require'true-zen.main'.main(0, 'on')]])
+			cmd([[command! TZBottomOff lua require'true-zen.main'.main(0, 'off')]])
+		end
+	end
 
-    if (opts["misc"]["ui_elements_commands"] == true) then
-        cmd([[command! TZBottom lua require'true-zen.main'.main(0, 'toggle')]])
-        cmd([[command! TZTop lua require'true-zen.main'.main(1, 'toggle')]])
-        cmd([[command! TZLeft lua require'true-zen.main'.main(2, 'toggle')]])
-    end
+	if opts["misc"]["ui_elements_commands"] == true then
+		cmd([[command! TZBottom lua require'true-zen.main'.main(0, 'toggle')]])
+		cmd([[command! TZTop lua require'true-zen.main'.main(1, 'toggle')]])
+		cmd([[command! TZLeft lua require'true-zen.main'.main(2, 'toggle')]])
+	end
 end
 
 local function setup_cursor(opts)
-    if (opts["misc"]["cursor_by_mode"] == true) then
-        cmd("set guicursor=i-c-ci:ver25,o-v-ve:hor20,cr-sm-n-r:block")
-    end
+	if opts["misc"]["cursor_by_mode"] == true then
+		cmd("set guicursor=i-c-ci:ver25,o-v-ve:hor20,cr-sm-n-r:block")
+	end
 end
 
 function M.setup(custom_opts)
-    require("true-zen.config").set_options(custom_opts)
+	require("true-zen.config").set_options(custom_opts)
 
 	-- must be required after or else it will stick to the default options
 	local opts = require("true-zen.config").options
-    setup_commands(opts)
-    setup_cursor(opts)
+	setup_commands(opts)
+	setup_cursor(opts)
 end
 
 return M
--- lua/true-zen/main.lua
+++ lua/true-zen/main.lua
@@ -14,22 +14,22 @@ local focus_mode = require("true-zen.services.modes.mode-focus.init")
 local cmd = vim.cmd
 
 function M.main(option, command_option)
-    option = option or 0
-    command_option = command_option or 0
+	option = option or 0
+	command_option = command_option or 0
 
-    if (option == 0) then
-        bottom.main(command_option)
-    elseif (option == 1) then
-        top.main(command_option)
-    elseif (option == 2) then
-        left.main(command_option)
-    elseif (option == 3) then
-        minimalist_mode.main(command_option)
-    elseif (option == 4) then
-        ataraxis_mode.main(command_option)
-    elseif (option == 5) then
-        focus_mode.main(command_option)
-    end
+	if option == 0 then
+		bottom.main(command_option)
+	elseif option == 1 then
+		top.main(command_option)
+	elseif option == 2 then
+		left.main(command_option)
+	elseif option == 3 then
+		minimalist_mode.main(command_option)
+	elseif option == 4 then
+		ataraxis_mode.main(command_option)
+	elseif option == 5 then
+		focus_mode.main(command_option)
+	end
 end
 
 return M
